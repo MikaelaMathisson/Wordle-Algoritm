a6384df34151060365cc968a1a98491f1c67c717
MikaelaMathisson <128062820+MikaelaMathisson@users.noreply.github.com>
2024-03-13 13:10:23 +0100

    refactoring

--- algorithmA.js
+++ algorithmA.js
@@ -1,41 +1,24 @@
 function algorithmA(guess, correctWord) {
-  //This makes the guess and correct word case sensitive
   guess = guess.toUpperCase();
   correctWord = correctWord.toUpperCase();
 
-  //Array that will store feedback
-  let result = [];
-
-  //Loop that goes through each position of the correct word
-  for (let i = 0; i < correctWord.length; i++) {
+  return Array.from({ length: correctWord.length }, (_, i) => {
     const guessedLetter = guess[i];
     const correctLetter = correctWord[i];
 
-    //Finds the index of the guessed letter in the correct word
-    const correctIndex = correctWord.indexOf(guessedLetter);
-
-    //Compares if the guessed letter is correct,
-    // if so it adds the result with correct as result
     if (guessedLetter === correctLetter) {
-      result.push({ letter: guessedLetter, result: "correct" });
+      return { letter: guessedLetter, result: "correct" };
     }
-    // Checks if the guessed letter is incorrect. Uses split to convert string into array,
-    // uses filter to see if theres more of the same letter, as L in Richards example,
-    // Making two Ls become one L with correct and one with incorrect,
-    // if theres only one L in the correct word
-    else if (
-      guess.split("").filter((char) => char === guessedLetter).length >
-      correctWord.split("").filter((char) => char === guessedLetter).length
-    ) {
-      result.push({ letter: guessedLetter, result: "incorrect" });
-    } else if (correctIndex !== i) {
-      result.push({ letter: guessedLetter, result: "misplaced" });
-    } else {
-      result.push({ letter: guessedLetter, result: "incorrect" });
-    }
-  }
 
-  return result;
+    const isIncorrect =
+      guess.split("").filter((char) => char === guessedLetter).length >
+      correctWord.split("").filter((char) => char === guessedLetter).length;
+
+    return {
+      letter: guessedLetter,
+      result: isIncorrect ? "incorrect" : "misplaced",
+    };
+  });
 }
 
 const testRun = algorithmA("HALLÅ", "CYKLA");
